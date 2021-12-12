22e8cfa3771bb6f91cb0bc659fae81c61491e1cd
Balazs Varga <balazs.varga023@gmail.com>
2021-12-12 12:56:55 +0100

    adding modified seeds file

--- seeds/index.js
+++ seeds/index.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 const Campground = require('../models/campground')
 const cities = require('./cities')
 const { places, descriptors } = require('./seedHelpers')
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp')
+mongoose.connect(dbUrl)
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error"));
